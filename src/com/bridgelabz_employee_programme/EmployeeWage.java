a621dd76e002e37ba22174baebef49c1edf509e7
Sanket Garde <sanketgarde7473@gmail.com>
2023-11-08 11:20:43 +0530

    Added : Java Functionality to calculate daily employee wage

--- src/com/bridgelabz_employee_programme/EmployeeWage.java
+++ src/com/bridgelabz_employee_programme/EmployeeWage.java
@@ -3,13 +3,19 @@ package com.bridgelabz_employee_programme;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to the employee payroll programme");
-
+        int Is_Full_time=1;
+        int Emp_Hrs=0,Emp_Wage=0;
+        int Emp_Rate_Per_hour=20;
         double empCheck = Math.floor(Math.random() * 10) % 2;
         System.out.println("Employee Attendance check " + empCheck);
-        if (empCheck == 1) {
+        if (empCheck == Is_Full_time) {
             System.out.println("Employee is present");
+            Emp_Hrs=8;
         } else {
             System.out.println("Employee is Absent");
+            Emp_Hrs=0;
         }
+        Emp_Wage=Emp_Hrs*Emp_Rate_Per_hour;
+        System.out.println("Employee wage is "+Emp_Wage);
     }
 }
