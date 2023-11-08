7fc56348a046bd5e568eee7206549d9b8cbe7ed4
Sanket Garde <sanketgarde7473@gmail.com>
2023-11-08 10:53:47 +0530

    Added : Java Programme To Display Message in employee payroll programme

---
+++ src/com/bridgelabz_employee_programme/EmployeeWage.java
@@ -0,0 +1,7 @@
+package com.bridgelabz_employee_programme;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the employee payroll programme");
+    }
+}
