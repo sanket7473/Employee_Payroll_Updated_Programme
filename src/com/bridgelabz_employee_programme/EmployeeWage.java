7b87360cb646a60395fac2b94ca3c3a3501d38e4
Sanket Garde <sanketgarde7473@gmail.com>
2023-11-08 12:27:39 +0530

    Added : Java Functionality to calculate daily employee wage For a Month

--- src/com/bridgelabz_employee_programme/EmployeeWage.java
+++ src/com/bridgelabz_employee_programme/EmployeeWage.java
@@ -1,28 +1,33 @@
 package com.bridgelabz_employee_programme;
 
 public class EmployeeWage {
-    static final int Is_Full_time=1;
-    static final int Is_Part_Time=2;
+   public static final int Is_Full_time=1;
+    public static final int Is_Part_Time=2;
+    public static final int Emp_Rate_Per_hour=20;
+    public static final int Num_Working_Days=20;
     public static void main(String[] args) {
         System.out.println("Welcome to the employee payroll programme");
-        int Emp_Hrs=0,Emp_Wage=0;
-        int Emp_Rate_Per_hour=20;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        System.out.println("Employee Attendance check " + empCheck);
-        switch((int) empCheck) {
-            case Is_Full_time:
-                System.out.println("Employee is present");
-                Emp_Hrs = 8;
-                break;
-            case Is_Part_Time:
-                System.out.println("Employee is present");
-                Emp_Hrs = 4;
-                break;
-            default:
-                Emp_Hrs = 0;
+        int Emp_Hrs = 0, Emp_Wage = 0,totalEmployeeWage=0;
+        for (int day = 1; day < Num_Working_Days; day++) {
+            double empCheck = Math.floor(Math.random() * 10) % 3;
+            System.out.println("Employee Attendance check " + empCheck);
+            switch ((int) empCheck) {
+                case Is_Full_time:
+                    System.out.println("Employee is present");
+                    Emp_Hrs = 8;
+                    break;
+                case Is_Part_Time:
+                    System.out.println("Employee is present");
+                    Emp_Hrs = 4;
+                    break;
+                default:
+                    Emp_Hrs = 0;
             }
-            Emp_Wage=Emp_Hrs*Emp_Rate_Per_hour;
-            System.out.println("Employee wage is "+Emp_Wage);
+            Emp_Wage = Emp_Hrs * Emp_Rate_Per_hour;
+            System.out.println("Employee wage is " + Emp_Wage);
+            totalEmployeeWage=totalEmployeeWage+Emp_Wage;
         }
+        System.out.println("Total Employee wage"+totalEmployeeWage);
+    }
 
     }
